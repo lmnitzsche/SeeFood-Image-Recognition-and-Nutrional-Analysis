b77ce66870fbb870e9015013a0f7c1c6206e8b97
lmnitzsche <logannitzsche1@gmail.com>
2024-11-30 15:31:33 -0600

    Sorting Processed Images (.jpg) for Classification

--- main.m
+++ main.m
@@ -15,24 +15,44 @@ laplacian_kernal = [0 -1 0; -1 4 -1; 0 -1 0];
 
 % --SCALE--
 scaled_image = scale_image(img, scaling_factor);
+
 % --SHARPEN--
 sharpened_image = sharpen_image(img, laplacian_kernal);
+
 % --WIENER FILTER--
-wiener_filtered_image = wiener_filter(img,1); %REPLACE IMG WITH THE BLURRED VERSION OF IMAGE AND REPLACE '1' WITH THE SIGMA VALUE THAT THE BLURRED IMAGE USES
+wiener_filtered_image = wiener_filter(img, 1); % Replace IMG with the blurred version of the image and replace '1' with the sigma value that the blurred image uses
 
 % --Gather image metrics here for experiments--
-calculateEME(img,scaled_image);
-calculateEME(img,wiener_filtered_image);
-
+calculateEME(img, scaled_image);
+calculateEME(img, wiener_filtered_image);
 
 % Display original, scaled, sharpened, wienered images 
 figure;
 subplot(1, 4, 1), imshow(img, []), title('Original Image (Grayscale)');
 subplot(1, 4, 2), imshow(scaled_image, []), title(['Scaled Image (Factor: ' num2str(scaling_factor) ')']);
 subplot(1, 4, 3), imshow(sharpened_image, []), title('Sharpened Image');
-subplot(1, 4, 4), imshow(wiener_filtered_image, []), title('Wiener Fitler Image');
+subplot(1, 4, 4), imshow(wiener_filtered_image, []), title('Wiener Filter Image');
 
-% Save the processed images as .jpg
-imwrite(im2uint8(mat2gray(scaled_image)), 'scaled_image.jpg'); % Save scaled image
-imwrite(im2uint8(mat2gray(sharpened_image)), 'sharpened_image.jpg'); % Save sharpened image
-imwrite(im2uint8(mat2gray(wiener_filtered_image)), 'wiener_filtered_image.jpg'); % Save Wiener filtered image
+% Ensure directories exist or create them
+if ~exist('scaled', 'dir')
+    mkdir('scaled');
+end
+if ~exist('sharpened', 'dir')
+    mkdir('sharpened');
+end
+if ~exist('wienered', 'dir')
+    mkdir('wienered');
+end
+
+% Get the original filename without extension
+[~, originalTitle, ~] = fileparts('cricket.png');
+
+% Save the processed images in respective folders
+scaledFilename = fullfile('scaled', [originalTitle '_scaled_factor_' num2str(scaling_factor) '.jpg']);
+imwrite(im2uint8(mat2gray(scaled_image)), scaledFilename); % Save scaled image
+
+sharpenedFilename = fullfile('sharpened', [originalTitle '_sharpened.jpg']);
+imwrite(im2uint8(mat2gray(sharpened_image)), sharpenedFilename); % Save sharpened image
+
+wienerFilename = fullfile('wienered', [originalTitle '_wiener_filtered.jpg']);
+imwrite(im2uint8(mat2gray(wiener_filtered_image)), wienerFilename); % Save Wiener filtered image
